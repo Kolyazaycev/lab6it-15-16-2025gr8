bf4737543eb6975cfcf1982dd3ef58df75829215
s5do <s5zn48ccr9@privaterelay.appleid.com>
2025-11-12 20:08:35 +0500

    Update lab6.cpp
    Инициализация генератора

--- lab6.cpp
+++ lab6.cpp
@@ -5,5 +5,12 @@
 #include "header.h"  
 using namespace std;
 int main() {
+
+  // Инициализация генератора случайных чисел
+    srand(time(0));
+
+    int userNumber;
+    bool isGuessed = false;
+    cout << "Добро пожаловать в игру 'Угадай число'!\n";
   
 }

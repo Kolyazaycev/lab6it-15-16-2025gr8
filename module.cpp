b42145999aa9b1476cf0d6dd557417411191006c
Kolyazaycev <watjajahaha@gmail.com>
2025-11-12 19:58:47 +0500

    Update module.cpp
    Функция проверки ввода

--- module.cpp
+++ module.cpp
@@ -7,3 +7,12 @@ using namespace std;
 int generateRandomNumber(int min, int max){  
     return rand() % (max - min + 1) + min;  
 }  
+// Функция проверки ввода пользователя
+bool checkInput(int& number){
+    if (!(cin >> number)) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        return false;
+    }
+    return true;
+}
